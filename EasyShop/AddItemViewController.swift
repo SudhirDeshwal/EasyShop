e5652d356be065d23312fca3666d8859ba770bcb
Sudhir Deshwal <DES>
2020-08-04 04:22:07 -0400

    controller and core data table created

---
+++ EasyShop/AddItemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddItemViewController.swift
+//  EasyShop
+//
+//  Created by user165333 on 8/4/20.
+//  Copyright © 2020 EasyShop. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
